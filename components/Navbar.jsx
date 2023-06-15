369395378344b877c7113a209d81dc5ff6cb28e0
manlikexavy <chibokaxavier@gmail.com>
2023-06-15 01:47:19 -0700

    NAVBAR COMPLETED NEXT IS BANNER

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from "react";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <nav className=" mt-10">
+      <div className="flex  justify-between">
+        <div className="flex space-x-6 items-center">
+          <div>Home</div>
+          <div>Portfolio</div>
+          <div>About</div>
+          <div></div>
+        </div>
+        <div className="flex  space-x-6 items-center">
+          <div>Skills</div>
+          <div>Services</div>
+          <div> Contact</div>
+        </div>
+      </div>
+    </nav>
+  );
+};
 
-export default Navbar
+export default Navbar;
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,14 @@
-import Navbar from '@/components/Navbar'
-import Image from 'next/image'
+import Navbar from "@/components/Navbar";
+import Image from "next/image";
 
 export default function Home() {
   return (
-  <main className=''>
-   <Navbar/>
-  </main>
-  )
-}
+  <>
+    <Navbar />
+   <main className="">
+    
+    </main>
+  </>
+   
+  );
+};
