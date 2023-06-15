03ef381e884025f7d7dd14df46a541e63f9af186
manlikexavy <chibokaxavier@gmail.com>
2023-06-15 10:52:58 -0700

    ABOUT COMPONENT DONE FOR NOW , UP NEXT IS THE PORTFOLIO SECTION

--- components/Banner.jsx
+++ components/Banner.jsx
@@ -6,12 +6,14 @@ import { motion } from "framer-motion";
 
 const Banner = () => {
   const [text, setText] = useTypewriter({
-    words: ["Hi, I’m Chiboka Emmanuel  A Software Engineer.","I offer engineering services such as FrontEnd Web Development & Web Design."],
-    loop: true,
-    delaySpeed: 2000,
+    words: [
+      "Hi, I’m Chiboka Emmanuel,  A Software Engineer.",
+    ],
+    loop: false,
+    delaySpeed: 5000,
   });
   return (
-    <div className="relative md:h-[95vh] h-[100vh]">
+    <div className="relative md:h-[100vh] h-[100vh]">
       {/* <div className="absolute top-0 left-0 -z-50 h-[100vh] md:w-[1140px]  w-full">
         <Image
           src="https://images.pexels.com/photos/3885344/pexels-photo-3885344.jpeg?auto=compress&cs=tinysrgb&w=600"
@@ -26,25 +28,40 @@ const Banner = () => {
       </div>
 
       <div className="flex items-center justify-center mt-[100px] text-center">
-            <motion.div
-              initial={{
-                x: 0,
-                opacity: 0,
-                scale: 0.5,
-              }}
-              animate={{
-                x: 0,
-                opacity: 1,
-                scale: 1,
-              }}
-              transition={{ duration: 1.5 }}
-              className="flex flex-col md:flex-row   items-center justify-between space-x-10 space-y-10 px-4"
-            >
-             <img src="https://images.pexels.com/photos/12513230/pexels-photo-12513230.jpeg?auto=compress&cs=tinysrgb&w=600" alt=""  className="w-[200px] h-[250px] md:w-[400px] md:h-[500px] rounded-lg"/>
-              <p className="text-lg italic text-gray-200 font-semibold">{text}  </p>
-            </motion.div>
-          
-        
+        <motion.div className="flex flex-col md:flex-row   items-center justify-between space-x-10 space-y-10 px-4">
+          <motion.img
+            initial={{
+              x: -500,
+              opacity: 0,
+              scale: 0.5,
+            }}
+            animate={{
+              x: 0,
+              opacity: 1,
+              scale: 1,
+            }}
+            transition={{ duration: 1.5 }}
+            src="https://images.pexels.com/photos/12513230/pexels-photo-12513230.jpeg?auto=compress&cs=tinysrgb&w=600"
+            alt=""
+            className="w-[200px] h-[250px] md:w-[400px] md:h-[500px] rounded-lg"
+          />
+          <motion.p
+            initial={{
+              x: 500,
+              opacity: 0,
+              scale: 0.5,
+            }}
+            animate={{
+              x: 0,
+              opacity: 1,
+              scale: 1,
+            }}
+            transition={{ duration: 1.5 }}
+            className="text-2xl text-gray-200 font-semibold"
+          >
+            {text}{" "}
+          </motion.p>
+        </motion.div>
       </div>
     </div>
   );
