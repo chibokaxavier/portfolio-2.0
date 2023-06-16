ba264b1a603d53cea752faec5834bc8fc5487d0e
manlikexavy <chibokaxavier@gmail.com>
2023-06-16 09:10:56 -0700

    ABOUT COMPONENT DONE

--- components/About.jsx
+++ components/About.jsx
@@ -1,40 +1,31 @@
 import React, { useEffect } from "react";
 
 const About = () => {
- 
   return (
-    <div className="h-screen ">
-      <div className=" flex flex-col md:flex-row justify-between items-center ">
-        <div className=" uppercase flex flex-col justify-between ">
-          <p className="md:text-[32px] text-[28px]  tracking-widest text-center font-bold">
+    <div className="h-screen">
+      <div className=" flex flex-col lg:flex-row justify-around   items-center  ">
+        <div className=" uppercase flex flex-col space-y-2 pt-28 lg:pt-0  lg:px-10 ">
+          <p className="md:text-[32px] text-[26px]   tracking-widest text-center   font-bold">
             About Me
           </p>
           <img
             src="https://technext.github.io/unfold/images/divider.png
 "
             alt="/"
-            className=""
+            className="pb-4"
           />
         </div>
-        <div className="md:w-[600px]">
-          <p className="tracking-[1px] text-[14px] md:text-left text-center  md:text-lg ">
+        <div className="md:w-[750px] lg:w-[600px] lg:mt-40  ">
+          <p className="tracking-[1px] text-[14px] sm:text-[16px] md:text-[25px] lg:text-[28px] md:text-left text-center  md:text-lg px-5">
             Hey there, I am Chiboka Emmanuel, an Undergraduate of the University
             of Nigeria, Nsukka. I am a passionate performance-driven Frontend
             Developer who has knowledge on HTML CSS, Javascript and Typescript.
             My personal strengths Includes
             resilience,punctuality,problem-solving,and the ability to work under
-            pressure. I welcome positively impactful opportunities where I can
-            explore technology and business in a progress-oriented organization
-            that welcomes and nurtures talent.I have an excellent reputation for
-            my integrity, productivity and leadership ability. I am also a very
-            fast learner, and i always strive to be and give the best in all i
-            do. I also have great Leadership skills, which has helped build my
-            character and shape me into the young man that i am today, i am big
-            on team work , because i believe that &quot;Two Heads are better than
-            one&quot; and i also believe that &quot;No Man is an Island&quot; meaning that no
-            matter how good or established a person is , he or she still needs
-            people around them, i think this is enough of me for now :) Thank
-            You.
+            pressure. I am also a very fast learner, and i always strive to be
+            and give the best in all i do. I also have great Leadership skills,
+            which has helped build my character and shape me into the young man
+            that i am today, i think this is enough of me for now :) Thank You.
           </p>
         </div>
       </div>
