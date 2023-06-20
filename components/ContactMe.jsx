ad712a43364fea3cc2ce38c465628fe9583138ad
manlikexavy <chibokaxavier@gmail.com>
2023-06-19 23:20:24 -0700

    CONTACT ME PAGE ALMOST DONE, REMINING THE SUBMIT FOIRM PART

--- components/ContactMe.jsx
+++ components/ContactMe.jsx
@@ -1,6 +1,19 @@
 import React from "react";
+import { useForm, SubmitHandler } from "react-hook-form";
 
 const ContactMe = () => {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (formData) => {
+    window.location.href = `mailto:chibokaxavier@gmail.com?subject='b'&body=Hi Emmanuel, my name is ${formData.name}. ${formData.message} (${formData.email})`;
+  };
+// const onSubmit = (formData) => {
+//     console.log(formData);
+//   };
   return (
     <div className="pb-20 ">
       <div className=" flex flex-col">
@@ -15,39 +28,65 @@ const ContactMe = () => {
             className="pb-4 w-[100px]"
           />
         </div>
-
-        <div className="flex">
-          <div>
-            <div className="pl-4 space-x-8 pb-8">
-              <input
-                type="text"
-                className="bg-transparent border-b  py-2 pr-2 outline-none w-[300px] text-gray-300 capitalize"
-                placeholder="NAME"
-              />
-              <input
-                type="email"
-                className="bg-transparent border-b  py-2 pr-2 outline-none w-[300px] text-gray-300 capitalize"
-                placeholder="EMAIL"
-              />
+        <form action="" onSubmit={handleSubmit(onSubmit)}>
+          <div className="lg:flex lg:space-x-20">
+            <div>
+              <div className="pl-4 lg:space-x-8 pb-8 relative">
+                <input
+                  {...register("name")}
+                  type="text"
+                  className="bg-transparent border-b text-sm md:text-xl lg:text-2xl py-2 pr-2 lg:mb-0 mb-16 outline-none  w-[85vw] lg:w-[300px] text-gray-300 capitalize"
+                  placeholder="NAME"
+                />
+                <input
+                  {...register("email")}
+                  type="email"
+                  className="bg-transparent border-b text-sm md:text-xl lg:text-2xl py-2 pr-2 outline-none lg:mb-0 mb-5 w-[85vw] lg:w-[300px] text-gray-300 capitalize"
+                  placeholder="EMAIL"
+                />
+              </div>
+              <div className="pl-4">
+                <textarea
+                  {...register("message")}
+                  className="bg-transparent border-b text-sm  md:text-xl lg:text-2xl py-2 pr-2 outline-none lg:w-[640px] w-[85vw] text-gray-300 capitalize"
+                  placeholder="WRITE YOUR MESSAGE"
+                  cols="50"
+                  rows="10"
+                ></textarea>
+              </div>
+              <div className="pt-10 pl-4 lg:hidden">
+                <button
+                  type="submit"
+                  className="uppercase rounded-full p-4 px-6 font-bold text-sm tracking-widest lg:hidden bg-transparent text-white outline-1 border"
+                >
+                  send message
+                </button>
+              </div>
             </div>
-            <div className="pl-4">
-              <textarea
-                className="bg-transparent border-b  py-2 pr-2 outline-none w-[640px] text-gray-300 capitalize"
-                placeholder="WRITE YOUR MESSAGE"
-                cols="50"
-                rows="10"
-              ></textarea>
+            <div className="flex flex-col space-y-10 justify-between pt-10 lg:items-start items-center">
+              <div className="flex justify-center flex-col items-center lg:items-start">
+                <p className="text-gray-400 text-xs ">EMAIL</p>
+                <p className="text-xl">Chibokaxavier@gmail.com</p>
+              </div>
+              <div className="flex justify-center flex-col items-center lg:items-start">
+                <p className="text-gray-400 text-xs">PHONE</p>
+                <p className="text-xl">+2349159521381</p>
+              </div>
+              <div className="flex justify-center flex-col items-center lg:items-start">
+                <p className="text-gray-400 text-xs">LOCATION</p>
+                <p className="text-xl">Lagos, Nigeria</p>
+              </div>
             </div>
           </div>
-          <div>
-            
+          <div className="pt-10 pl-4 hidden lg:block">
+            <button
+              type="submit"
+              className="uppercase rounded-full p-4 bg-transparent hidden lg:block text-white outline-1 border"
+            >
+              send message
+            </button>
           </div>
-        </div>
-      </div>
-      <div className="pt-10 pl-4">
-        <button className="uppercase rounded-full p-4 bg-transparent text-white outline-1 border">
-          send message
-        </button>
+        </form>
       </div>
     </div>
   );
