bbb2496bc345afb36f659fbc693e4c5683e7a9d1
manlikexavy <chibokaxavier@gmail.com>
2023-06-21 12:01:19 -0700

    .

--- components/Project.jsx
+++ components/Project.jsx
@@ -2,10 +2,11 @@ import React from "react";
 
 const Project = ({ Project }) => {
   return (
-    <div className="pt-10"> 
-          <p className="items-center flex justify-center text-xl">{Project.title}</p>
+    <div className="pt-10">
+      <p className="items-center flex justify-center text-xl">
+        {Project.title}
+      </p>
       <div className="flex lg:flex-row flex-col justify-between py-8 items-center">
-      
         <img src={Project.img} alt="/" className="lg:w-[50%] h-[]" />
         <p className="text-sm hidden lg:block">{Project.desc}</p>
       </div>
