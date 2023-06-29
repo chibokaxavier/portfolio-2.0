0bc95e95d40fe3ea53930eb6d67fa1b86a546f25
manlikexavy <chibokaxavier@gmail.com>
2023-06-29 09:46:36 -0700

    PROJECTS COMPONENT ANIMATIONS COMPLETED

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -1,17 +1,35 @@
-import React from "react";
+import React, { useEffect } from "react";
 import projects from "@/projectData";
 import Project from "./Project";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 const Projects = () => {
+  useEffect(() => {
+    AOS.init();
+  }, []);
   return (
     <div className="pt-28">
       <div className="flex flex-col items-start">
-        <p className="md:text-[7rem] lg:text-[8rem] text-[3.5rem]  header pl-4 tracking-widest text-left font-bold">
+        <p
+          data-aos="fade-left"
+          data-aos-offset="100"
+          // data-aos-delay="0"
+          data-aos-duration="500"
+          className="md:text-[7rem] lg:text-[8rem] text-[3.5rem]  header pl-4 tracking-widest text-left font-bold"
+        >
           PROJECTS
         </p>
         <div className="">
-          {projects.map((project,i) => (
-            <div className="my-10" key={i}>
+          {projects.map((project, i) => (
+            <div
+              data-aos="zoom-out"
+              data-aos-offset="200"
+              data-aos-delay="100"
+              data-aos-duration="2000"
+              className="my-10"
+              key={i}
+            >
               {" "}
               <Project Project={project} key={project.id} className="" />
             </div>
