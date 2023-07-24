da0ebfe0f137b629236bc18a32d978e642432f94
manlikexavy <chibokaxavier@gmail.com>
2023-07-24 11:33:10 -0700

    changed resume

--- components/About.jsx
+++ components/About.jsx
@@ -9,14 +9,14 @@ const About = () => {
   }, []);
   const onButtonClick = () => {
     // using Java Script method to get PDF file
-    fetch("Resume.pdf").then((response) => {
+    fetch("mainResume.pdf").then((response) => {
       response.blob().then((blob) => {
         // Creating new object of PDF file
         const fileURL = window.URL.createObjectURL(blob);
         // Setting various property values
         let alink = document.createElement("a");
         alink.href = fileURL;
-        alink.download = "Resume.pdf";
+        alink.download = "mainResume.pdf";
         alink.click();
       });
     });
